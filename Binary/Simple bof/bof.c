d78fdea39017828422479c419fcf8fbe37d94d32
Rishit Saiya <180010027@iitdh.ac.in>
2021-01-06 17:57:28 +0530

    Added Simple bof - Binary

---
+++ Binary/Simple bof/bof.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+// Defined in a separate source file for simplicity.
+void init_visualize(char* buff);
+void visualize(char* buff);
+void safeguard();
+
+void print_flag();
+
+void vuln() {
+  char padding[16];
+  char buff[32];
+  int notsecret = 0xffffff00;
+  int secret = 0xdeadbeef;
+
+  memset(buff, 0, sizeof(buff)); // Zero-out the buffer.
+  memset(padding, 0xFF, sizeof(padding)); // Zero-out the padding.
+
+  // Initializes the stack visualization. Don't worry about it!
+  init_visualize(buff); 
+
+  // Prints out the stack before modification
+  visualize(buff);
+
+  printf("Input some text: ");
+  gets(buff); // This is a vulnerable call!
+
+  // Prints out the stack after modification
+  visualize(buff); 
+
+  // Check if secret has changed.
+  if (secret == 0x67616c66) {
+    puts("You did it! Congratuations!");
+    print_flag(); // Print out the flag. You deserve it.
+    return;
+  } else if (notsecret != 0xffffff00) {
+    puts("Uhmm... maybe you overflowed too much. Try deleting a few characters.");
+  } else if (secret != 0xdeadbeef) {
+    puts("Wow you overflowed the secret value! Now try controlling the value of it!");
+  } else {
+    puts("Maybe you haven't overflowed enough characters? Try again?");
+  }
+
+  exit(0);
+}
+
+int main() {
+  setbuf(stdout, NULL);
+  setbuf(stdin, NULL);
+  safeguard();
+  vuln();
+}
